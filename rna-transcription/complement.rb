8302c2322bb2cd9a23bb962f514ad6855a94cfa1
Jeffrey Gu <jeffwgu@gmail.com>
2015-01-16 10:10:53 -0700

    further streamlined rna

--- rna-transcription/complement.rb
+++ rna-transcription/complement.rb
@@ -1,11 +1,9 @@
 class Complement
-  DNA_HASH = {G: "C", C: "G", T: "A", A: "U"}
-
   def self.of_dna(nucleo)
-    nucleo.chars.map {|letter| DNA_HASH[letter.to_sym]}.join
+    nucleo.tr("GCTA", "CGAU")
   end
 
   def self.of_rna(nucleo)
-    nucleo.chars.map {|letter| DNA_HASH.key(letter).to_s}.join
+    nucleo.tr("CGAU", "GCTA")
   end
 end
