4426515c8a21119582d0eda4d3664d7dc661e72d
Jeffrey Gu <jeffwgu@gmail.com>
2015-01-15 22:48:13 -0700

    fixed robo

--- robot-name/robot.rb
+++ robot-name/robot.rb
@@ -1,12 +1,11 @@
 class Robot
   attr_accessor :name
   def initialize
-    @new_name = true
     @name = self.name_generate
   end
 
   def name_generate
-    Array.new(2){("A".."Z").to_a.sample}.join + rand(999).to_s.rjust(3, "0")
+    Array.new(2){(("A".."Z").to_a + ("a".."z").to_a).sample}.join + rand(999).to_s.rjust(3, "0")
   end
 
   def reset
