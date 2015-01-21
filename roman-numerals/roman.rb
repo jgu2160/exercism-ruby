efb708162a1e24666550477332883a3f5747d8d7
Jeffrey Gu <jeffwgu@gmail.com>
2015-01-20 21:55:50 -0700

    refactored roman a little

--- roman-numerals/roman.rb
+++ roman-numerals/roman.rb
@@ -3,25 +3,28 @@ ROMAN_HASH = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000}
 public
 
 def to_roman
-  digit_array = self.to_s.split('').map { |digit| digit.to_i }.reverse
-  romanizing_block = Proc.new {|digit, index| digit.digitize(1 * 10 ** index, 5 * 10 ** index, 10 * 10 ** index)} 
+  digit_array = self.split_and_reverse
   digit_array.map.each_with_index(&romanizing_block).reverse.join
 end
 
-def digitize(one, five, ten)
+def split_and_reverse
+  self.to_s.split('').map { |digit| digit.to_i }.reverse
+end
+
+def romanizing_block
+  Proc.new {|digit, index| digit.categorize_digit_with_magnitude(1 * 10 ** index, 5 * 10 ** index, 10 * 10 ** index)} 
+end
+
+def categorize_digit_with_magnitude(one, five, ten)
   digit = self 
-  if digit < 4
-    digit.single_make(one)
-  elsif digit == 5
-    1.single_make(five)
-  elsif digit == 9
-    9.surround_x(ten, one)
-  else 
-    digit.surround_x(five, one)
+  if digit < 4 then digit.make_single_numeral(one)
+  elsif digit == 5 then 1.make_single_numeral(five)
+  elsif digit == 9 then 9.surround_x(ten, one)
+  else digit.surround_x(five, one)
   end
 end
 
-def single_make(single_type)
+def make_single_numeral(single_type)
   ROMAN_HASH.key(single_type).to_s * self
 end
 
@@ -29,8 +32,8 @@ def surround_x(x, surrounder)
   diff = self * surrounder - x
   diff_digits = diff.to_s.split('').map {|d| d.to_i}
   if diff < 0
-    1.single_make(surrounder) + 1.single_make(x)
+    1.make_single_numeral(surrounder) + 1.make_single_numeral(x)
   else 
-    1.single_make(x) + diff_digits[0].single_make(surrounder)
+    1.make_single_numeral(x) + diff_digits[0].make_single_numeral(surrounder)
   end
 end
